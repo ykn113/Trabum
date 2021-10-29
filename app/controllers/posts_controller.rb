6cdf786622ad1b7759fcc79900767dedfefeabbe
ykn113 <yukinosnj13@gmail.com>
2021-10-29 03:50:22 +0000

    [Add] Post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,18 @@
 class PostsController < ApplicationController
   def index
+    @continents = Continent.all
+    if params[:continent_id]
+      @continent = @continents.find(params[:continent_id])
+      @posts = @continent.posts.where(user_id: current_user.id).page(params[:page]).per(6)
+    else
+      @posts = current_user.posts.page(params[:page]).per(6).order(id: "DESC")
+    end
+    @all_posts_count = @posts.total_count
   end
 
   def show
+    @post = Post.find(params[:id])
+    redirect_to users_path unless current_user == @post.user
   end
 
   def new
@@ -22,6 +32,30 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+    @continent_list = Continent.pluck('name, id')
+    if @post.user.id == current_user.id
+      render :edit
+    else
+      redirect_to :posts_path
+    end
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    @continents = Continent.all
+    if @post.update(post_params)
+      redirect_to post_path(@post.id), notice: '写真を更新しました。'
+    else
+       @continent_list = Continent.pluck('name, id')
+      render :edit
+    end
+  end
+  
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :continent
   attachment :image
+  
+  validates :country, :city, :flight, presence: true, length: { maximum: 20 }
+  validates :image, :from_date, :to_date, :continent_id, presence: true
 end
