12790f44742c59bb8bd770bd660042f78e705150
ykn113 <yukinosnj13@gmail.com>
2021-11-02 09:14:51 +0000

    [Add] Unsubscribe

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
   skip_before_action :require_sign_in!, only: [:new, :create]
   before_action :set_user, only: [:create]
-
+  before_action :reject_user, only: [:create]
 
   def new
   end
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
       sign_in(@user)
       redirect_to users_path
     else
-      flash.now[:danger] = t('.flash.invalid_password')
+      flash[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
@@ -21,6 +21,8 @@ class SessionsController < ApplicationController
     sign_out
     redirect_to root_path
   end
+  
+  
 
   private
 
@@ -33,6 +35,28 @@ class SessionsController < ApplicationController
 
   def session_params
     params.require(:session).permit(:email, :password)
+  end
+  
+  def reject_inactive_user
+    @user = current_user
+    if @user
+      if @user.valid_password?(params[:user][:password]) && !@user.is_valid
+        reset_session
+        redirect_to signup_path
+      end
+    end
+  end
+  
+  def reject_user
+    @user = User.find_by(email: params[:user][:email].downcase)
+    if @user
+      if (@user.valid_password?(params[:user][:password]) && (@user.active_for_authentication? == false))
+        flash[:error] = "退会済みです。"
+        redirect_to new_user_session_path
+      end
+    else
+      flash[:error] = "必須項目を入力してください。"
+    end
   end
 
 end
