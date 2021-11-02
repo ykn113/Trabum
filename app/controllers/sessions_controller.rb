96d7bfb7dda2698864f25cad80764a9691abac92
ykn113 <yukinosnj13@gmail.com>
2021-11-02 10:29:02 +0000

    [Edit] Unsubscribe

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ class SessionsController < ApplicationController
 
   skip_before_action :require_sign_in!, only: [:new, :create]
   before_action :set_user, only: [:create]
-  before_action :reject_user, only: [:create]
 
   def new
   end
@@ -21,8 +20,6 @@ class SessionsController < ApplicationController
     sign_out
     redirect_to root_path
   end
-  
-  
 
   private
 
@@ -44,18 +41,6 @@ class SessionsController < ApplicationController
         reset_session
         redirect_to signup_path
       end
-    end
-  end
-  
-  def reject_user
-    @user = User.find_by(email: params[:user][:email].downcase)
-    if @user
-      if (@user.valid_password?(params[:user][:password]) && (@user.active_for_authentication? == false))
-        flash[:error] = "退会済みです。"
-        redirect_to new_user_session_path
-      end
-    else
-      flash[:error] = "必須項目を入力してください。"
     end
   end
 
