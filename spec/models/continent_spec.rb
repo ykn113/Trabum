a55a2fce1d929c627783755b36d7042cbb6415ca
ykn113 <yukinosnj13@gmail.com>
2021-11-03 07:37:51 +0000

    [Add] Rspec

---
+++ spec/models/continent_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "continentモデルに関するテスト", type: :model do
+  describe 'アソシエーションのテスト' do
+    context 'postモデルとの関係' do
+      it '1:Nとなっている' do
+        expect(User.reflect_on_association(:posts).macro).to eq :has_many
+      end
+    end
+  end
+end
